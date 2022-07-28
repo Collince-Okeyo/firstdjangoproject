90b3ee84661ac6dda6278af2f71fcc8c3e831a65
Collince-Okeyo <collinceokeyo98@gmail.com>
2022-07-28 17:17:03 +0300

    Did user auth pages

--- accounts/views.py
+++ accounts/views.py
@@ -1,28 +1,61 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
+from django.urls import is_valid_path
+from sqlalchemy import null
 from .models import *
-from .forms import OrderForm 
+from .forms import CreateUserForm, OrderForm 
 from .filters import OrderFilter 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+
+# Adding message
+from django.contrib import messages
+
 
 
 # Create your views here.
+
+def registerPage(request):
+	form = CreateUserForm()
+
+	if request.method == "POST":
+		form = CreateUserForm(request.POST)
+		if form.is_valid():
+			form.save()
+			user = form.cleaned_data.get('username')
+			messages.success(request, 'Account was created successfuly for '+ user)
+			return redirect('login')
+	context = {'form': form}
+	return render(request, 'accounts/register.html', context)
+
+def loginPage(request):
+
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+
+		user = authenticate(request, username=username, password=password)
+
+		if user is not None:
+			login(request, user)
+			return redirect('home')
+
+	context = {}
+	return render(request, 'accounts/login.html', context)
 
 # Rendering templates
 def home(request):
 	# Showing the data in the home
 	orders = Order.objects.all()
 	customers = Customer.objects.all()
-
 	totoal_customers = customers.count()
 
 # Rendering data to templates
 	total_orders = orders.count()
 	delivered = orders.filter(status="Delivered").count()
 	pending = orders.filter(status="Pending").count()
-
 	context = {'orders':orders, 'customers':customers, 'total_orders':total_orders, 'delivered':delivered, 'pending':pending}
-
 	return render(request, 'accounts/dashboard.html', context)
 
 def products(request):
@@ -32,13 +65,10 @@ def products(request):
 
 def customer(request, pk_test):
 	customer = Customer.objects.get(id=pk_test)
-
 	orders = customer.order_set.all()
 	orders_total = orders.count()
-
 	myFilter = OrderFilter(request.GET, queryset=orders)
 	orders = myFilter.qs
-
 	context = {"customer": customer, 'orders': orders, 'orders_total': orders_total, 'myFilter': myFilter}
 	return render(request, 'accounts/customer.html', context)
 
@@ -56,18 +86,16 @@ def createOrder(request, pk):
 		if formSet.is_valid():
 			formSet.save()
 			return redirect('/')
-	
 	context = {'formSet': formSet}
 	return render(request, 'accounts/order_form.html', context)
 
 def updateOrders(request, pk):
-
 	order = Order.objects.get(id=pk)
 	form = OrderForm(instance=order)
-
 	if request.method == 'POST':
 		# print('Printing POST: ', request.POST)
 		form = OrderForm(request.POST, instance=order)
+
 		if form.is_valid():
 			form.save()
 			return redirect('/')
